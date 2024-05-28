97283aa1672ee81bf33702508c16318cdf4cec38
Evandro Calado <evandrocalado07@gmail.com>
2024-05-27 21:00:29 -0300

    🎉 init: initial commit

---
+++ src/styles/globals.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+    scroll-behavior: smooth;
+    font-family: "Lato", sans-serif;
+  }
+
+  body {
+    font-size: 1.6rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+`;
