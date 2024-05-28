bf911549b1af4db2d2bdd45b40808539960f7f00
Evandro Calado <evandrocalado07@gmail.com>
2024-05-27 22:41:49 -0300

    🎉 init: initial commit

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -11,6 +11,7 @@ export const GlobalStyles = createGlobalStyle`
     font-size: 62.5%;
     scroll-behavior: smooth;
     font-family: "Lato", sans-serif;
+    color: ${({ theme }) => theme.colors.primary};
   }
 
   body {
