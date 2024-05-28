deae456c49a5889aac132f543bcbebdc1a2d2749
Evandro Calado <evandrocalado07@gmail.com>
2024-05-27 22:46:38 -0300

    🐳 chore: add stories

---
+++ src/components/Input/Input.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import { Input, InputProps } from '.';
+
+export default {
+  title: 'Components/Input',
+  component: Input,
+} as Meta;
+
+export const Inputs: StoryFn<InputProps> = () => (
+  <div style={{ display: 'flex', gap: '6rem', flexWrap: 'wrap' }}>
+    <Input type="text" label="text" name="text" />
+    <Input type="email" label="email" name="email" />
+    <Input type="password" label="password" name="password" />
+    <Input type="number" label="number" name="number" />
+    <Input type="date" label="date" name="date" />
+    <Input type="text" label="disabled" name="disabled" disabled />
+  </div>
+);
