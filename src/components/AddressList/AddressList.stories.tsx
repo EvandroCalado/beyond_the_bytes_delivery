71c24fbe9f9cb20224294019ffa29c688af659db
Evandro Calado <evandrocalado07@gmail.com>
2024-05-29 10:54:39 -0300

    🐳 chore: add args

--- src/components/AddressList/AddressList.stories.tsx
+++ src/components/AddressList/AddressList.stories.tsx
@@ -5,9 +5,23 @@ import { AddressList, AddressListProps } from '.';
 export default {
   title: 'Components/AddressList',
   component: AddressList,
+  args: {
+    addresses: [
+      {
+        lot: '1234',
+        details: 'monitor',
+      },
+      {
+        lot: '4321',
+        details: 'placa mãe',
+      },
+    ],
+  },
   parameters: {
     layout: 'fullscreen',
   },
 } as Meta;
 
-export const Default: StoryFn<AddressListProps> = () => <AddressList />;
+export const Default: StoryFn<AddressListProps> = (args) => (
+  <AddressList {...args} />
+);
