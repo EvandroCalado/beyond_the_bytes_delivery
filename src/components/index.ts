4e74fa233754c53958fba184c32db18d7688c8fc
Evandro Calado <evandrocalado07@gmail.com>
2024-05-29 20:21:19 -0300

    🐳 chore: remove counter

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './AddressForm';
 export * from './AddressList';
 export * from './Button';
-export * from './Counter';
 export * from './EditAddress';
 export * from './Heading';
 export * from './Input';
