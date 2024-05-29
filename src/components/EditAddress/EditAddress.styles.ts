7cb57a1a5c0a157007eda9715ceae4c0fac1d0fc
Evandro Calado <evandrocalado07@gmail.com>
2024-05-29 10:53:05 -0300

    🐞 fix: remove theme unused

--- src/components/EditAddress/EditAddress.styles.ts
+++ src/components/EditAddress/EditAddress.styles.ts
@@ -1,9 +1,7 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  ${({ theme }) => css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `}
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
