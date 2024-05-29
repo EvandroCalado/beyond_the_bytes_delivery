cbd9af56042dc21d516da8a861d655c3220bb338
Evandro Calado <evandrocalado07@gmail.com>
2024-05-28 23:28:35 -0300

    ✨ feat: create address list

---
+++ src/components/AddressList/AddressList.styles.ts
@@ -0,0 +1,70 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Addresses = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: ${theme.spacings.lg};
+    padding: 0 ${theme.spacings.md};
+    padding: ${theme.spacings.md};
+    border: 1px solid ${theme.colors.gray};
+    border-radius: ${theme.spacings.sm};
+
+    & > h1 {
+      text-transform: capitalize;
+
+      @media ${theme.media.lteOrEqMedium} {
+        font-size: ${theme.font.sizes.lg};
+      }
+    }
+
+    & > ul {
+      display: flex;
+      flex-direction: column;
+    }
+
+    & > ul > li {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: ${theme.font.sizes.xs};
+      padding: ${theme.spacings.md};
+      border-radius: ${theme.spacings.xs};
+
+      & > .address {
+        display: flex;
+        gap: ${theme.spacings.xs};
+        text-transform: capitalize;
+
+        & > span {
+          font-weight: 800;
+        }
+      }
+
+      & > button {
+        padding: ${theme.spacings.xs};
+      }
+    }
+
+    & > ul > li:nth-child(odd) {
+      background-color: ${theme.colors.secondary};
+    }
+
+    & > span {
+      font-size: ${theme.font.sizes.lg};
+    }
+
+    & > span::first-letter {
+      text-transform: capitalize;
+    }
+  `}
+`;
---
+++ src/components/AddressList/AddressList.tsx
@@ -0,0 +1,51 @@
+import { Pencil } from 'lucide-react';
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Button, Heading } from 'components';
+
+import * as S from './AddressList.styles';
+
+interface Address {
+  lot: string;
+  details: string;
+}
+
+export interface AddressListProps {
+  addresses: Address[];
+}
+
+export const AddressList: FC<AddressListProps> = ({ addresses }) => {
+  const navigate = useNavigate();
+
+  return (
+    <S.Container>
+      <S.Addresses>
+        <Heading as={'h1'} size="xl" fontWeight="800">
+          lista de endereços
+        </Heading>
+
+        {addresses.length === 0 && <span>não há endereços</span>}
+
+        <ul>
+          {addresses.length > 0 &&
+            addresses.map((address, index) => (
+              <li key={index}>
+                <div className="address">
+                  <span>Lote:</span> {address.lot}
+                  <span>Detalhes:</span> {address.details}
+                </div>
+                <Button onClick={() => navigate(`/edit-address/${index}`)}>
+                  <Pencil size={16} />
+                </Button>
+              </li>
+            ))}
+        </ul>
+
+        <Button onClick={() => navigate('/register-address')}>
+          novo endereço
+        </Button>
+      </S.Addresses>
+    </S.Container>
+  );
+};
