926eea680c15983bbad50f7b3ff136e700ef0ab8
Evandro Calado <evandrocalado07@gmail.com>
2024-05-29 10:53:33 -0300

    🐳 chore: add args and params

--- src/components/EditAddress/EditAddress.stories.tsx
+++ src/components/EditAddress/EditAddress.stories.tsx
@@ -5,9 +5,30 @@ import { EditAddress, EditAddressProps } from '.';
 export default {
   title: 'Components/EditAddress',
   component: EditAddress,
+  args: {
+    addresses: [
+      {
+        lot: '1234',
+        details: 'monitor',
+      },
+      {
+        lot: '4321',
+        details: 'placa',
+      },
+    ],
+    updateAddress: () => {},
+  },
   parameters: {
     layout: 'fullscreen',
+    reactRouter: {
+      routePath: '/edit-address/:id',
+      routeParams: {
+        id: '0',
+      },
+    },
   },
 } as Meta;
 
-export const Default: StoryFn<EditAddressProps> = () => <EditAddress />;
+export const Default: StoryFn<EditAddressProps> = (args) => (
+  <EditAddress {...args} />
+);
